a9470ea9bc1cb44ffe770b7ecf55c3f8b136ffad
feketeboti <103440117+feketeboti@users.noreply.github.com>
2022-07-15 14:57:26 +0200

    Add files via upload

---
+++ UDP_Server.py
@@ -0,0 +1,42 @@
+import socket
+
+localIP = str([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]) # https://stackoverflow.com/a/1267524
+
+localPort = 8080
+
+bufferSize = 256
+
+msgFromServer = "Data Transfer is running"
+
+bytesToSend = str.encode(msgFromServer)
+
+# Create a datagram socket
+
+UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+# Bind to address and ip
+
+UDPServerSocket.bind((localIP, localPort))
+
+print("UDP server up and listening")
+
+print(localIP)
+
+# Listen for incoming datagrams
+
+while (True):
+    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+
+    message = bytesAddressPair[0]
+
+    address = bytesAddressPair[1]
+
+    clientMsg = "Message from Client:{}".format(message)
+    clientIP = "Client IP Address:{}".format(address)
+
+    print(clientMsg)
+    print(clientIP)
+
+    # Sending a reply to client
+
+    UDPServerSocket.sendto(bytesToSend, address)
